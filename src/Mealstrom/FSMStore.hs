df562b2a338b5c8f27dc2a872d4030293ea6d6cb
Max Amanshauser <max@lambdalifting.org>
2016-12-06 17:13:37 +0800

    correct docs

--- src/Mealstrom/FSMStore.hs
+++ src/Mealstrom/FSMStore.hs
@@ -15,8 +15,8 @@ import Mealstrom.FSM
 data Proxy k s e a = Proxy
 
 -- |Even the Read method needs type parameters 'e' and 'a' because it needs to deserialise the entry from storage.
--- Implementations are expected to not throw exceptions in fsmRead.
--- Throwing in fsmCreate/fsmUpdate is OK.
+-- Implementations are expected to not throw exceptions in fsmRead/fsmCreate.
+-- Throwing in fsmUpdate is OK.
 class FSMStore st k s e a where
     fsmRead   :: st -> k -> Proxy k s e a -> IO (Maybe s)
     fsmCreate :: st -> Instance k s e a -> IO (Maybe String)
